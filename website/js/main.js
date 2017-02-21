e3f663e2a23b21fc217f8acecc3bd6308223616c
lukasmoerchen <lukas@moerchen.fr>
2017-02-21 23:25:39 +0100

    Korrigiert Header Burger-Button
    Der Burger im responsiven Header wird korrigiert.

--- website/js/main.js
+++ website/js/main.js
@@ -15,3 +15,21 @@ $(function() {
 	}
 
 });
+
+function header() {
+      var x = document.getElementById("header");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
+  }
+
+function headerch() {
+      var x = document.getElementById("header-ch");
+      if (x.className === "topnav") {
+          x.className += " responsive";
+      } else {
+          x.className = "topnav";
+      }
+  }
