a87d5a57e11b975a25a84205f603c97b2d8f569b
lukasmoerchen <lukas@moerchen.fr>
2017-02-22 12:29:43 +0100

    Kontaktformular überarbeitet und responsive Bilder auf Startseite

---
+++ website/action_page.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?><?php echo $_POST["lastname"]; ?><br>
+You are from: <?php echo $_POST["country"]; ?><br>
+Your message: <?php echo $_POST["subject"]; ?><br>
+
+</body>
+</html>
