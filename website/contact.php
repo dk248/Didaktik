b536f5c30dfee75980f9dd7ee2db6161f695c39c
lukasmoerchen <lukas@moerchen.fr>
2017-02-25 17:10:35 +0100

    Entfernt überflüssige Dateien

--- website/contact.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Welcome <?php echo $_POST["firstname"]; ?><?php echo $_POST["lastname"]; ?><br>
-You are from: <?php echo $_POST["country"]; ?><br>
-Your message: <?php echo $_POST["subject"]; ?><br>
-
-</body>
-</html>
